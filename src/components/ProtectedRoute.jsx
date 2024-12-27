56abdb602016461566c2b7f5f03852d64568eb0f
hp <zekariastamiru12@gmail.com>
2024-12-27 21:41:20 +0300

    add ProtectedRoute component for authentication handling

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(
+    function () {
+      if (!isAuthenticated) navigate("/");
+    },
+    [isAuthenticated, navigate]
+  );
+  return children;
+}
+
+export default ProtectedRoute;
